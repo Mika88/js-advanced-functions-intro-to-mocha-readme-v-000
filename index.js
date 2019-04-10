d89e9340d43385a7a5882a1ad48c1a3cf53acfde
Mika88 <mikaspivak@gmail.com>
2019-04-10 20:47:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function favoriteIceCream(flavor) {
   return `I love ${flavor}`;
 }
 
-function shouting(sentance) {
-  sentance.toUpperCase();
+function shouting(sentence) {
+  sentence.toUpperCase();
 }
