97c43e536782d5439d698098abc90632d431537f
Mika88 <mikaspivak@gmail.com>
2019-04-10 20:44:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function sayHey() {
 }
 
 function sayHeyFriend(name) {
-  return `Hey, ${name}!`
+  return `Hey, ${name}!`;
 }
 
 function favoriteIceCream(flavor) {
-  return `I love ${flavor}`
+  return `I love ${flavor}`;
 }
